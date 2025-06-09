278a026ea5d1d7dfe17e40cc880c3793b8139f22
Benappiahpoku <benappiahpoku@gmail.com>
2025-06-09 10:33:15 +0000

    phone icon removed

--- src/views/ReceiptPreview.vue
+++ src/views/ReceiptPreview.vue
@@ -19,13 +19,10 @@
                 <h3 class="text-2xl font-bold text-gray-800">{{ companyName || 'Your Company Name' }}</h3>
                 <!-- ===== [Fix: Align phone icon and number] ===== -->
                 <div class="inline-flex items-center justify-center text-gray-600 gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor"
-                        style="display: inline-block; vertical-align: middle;">
-                        <path
-                            d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                    </svg>
+
                     <span class="inline-block" style="vertical-align: middle; line-height: 1;">
                         {{ phoneNumber || '024 XXX XXXX' }}
+                        
                     </span>
                 </div>
             </div>
